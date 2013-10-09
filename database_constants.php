93b7e299e9a8c30cad1a8bfb57cdf82a11c2e0c8
siriuzwhite <siriuz@gmx.net>
2013-10-09 08:47:36 +0200

    v0.7.1.1

---
+++ database_constants.php
@@ -0,0 +1,20 @@
+<?php
+/*********************************************************************************
+ *
+ *                           A C H T U N G !
+ *
+ *  ||  Bitte verändern Sie die folgenden Datenbankkonstanten zu der        ||
+ *  \/  MySQL Datenbank und dem MySQL-Benutzer, den Sie erstellt haben.     \/
+ *********************************************************************************
+ */
+define("OBIB_HOST",     "localhost");
+define("OBIB_DATABASE", "bibo");
+define("OBIB_USERNAME", "bibo");
+define("OBIB_PWD",      "obib");
+define("MAIN_LOCALE",   "de"); // de oder en
+/*********************************************************************************
+ *  /\                                                                      /\
+ *  ||                                                                      ||
+ *********************************************************************************
+ */
+?>
---
+++ locale/de/sql/0.4.0/domain/staff.sql
@@ -0,0 +1,17 @@
+insert into %prfx%staff
+values (null
+  ,sysdate()
+  ,sysdate()
+  ,1
+  ,'admin'
+  ,password('admin')
+  ,'Administrator'
+  ,null
+  ,'N'
+  ,'Y'
+  ,'Y'
+  ,'Y'
+  ,'Y'
+  ,'Y'
+)
+;
